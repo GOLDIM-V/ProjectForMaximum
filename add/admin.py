78040e79c11b8528dd6e8d7878972321a849f014
Slava <slavaurty@mail.ru>
2023-08-14 16:12:12 +0800

    дз 9.4.1

--- add/admin.py
+++ add/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from .models import Advert
+from django.utils.html import format_html
+
 
 # Register your models here.
 
 class AdvertAdmin(admin.ModelAdmin):
-    list_display = ["id", "title", "description", "price", "auction", "date_of_creation", "date_of_update"]
+    list_display = ["id", "title", "description", "price", "auction", "date_of_creation", "date_of_update", 'get_image']
     list_filter = ["auction", "created_at"]
     actions = ["make_auctions_as_true", "make_auctions_as_false"]
     fieldsets = (
         ("общее", {
-              'fields': ("title", "description")
+              'fields': ("title", "description", "user", "image")
         }),
 
         ("финансы", {
@@ -17,7 +19,11 @@ class AdvertAdmin(admin.ModelAdmin):
             "classes": ['collapse']
         })
     )
-    
+
+    @admin.display(description='картинка')
+    def get_image(self, obj):
+        return format_html('<img src="{}", width=150, hight=100>', obj.image.url)
+
 
     @admin.action(description="Добавить возможность торга")
     def make_auctions_as_true(self, request, queuryset):
