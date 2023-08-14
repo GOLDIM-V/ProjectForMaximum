d93e4560b2185c664edef16f22d645861ebc1370
Slava <slavaurty@mail.ru>
2023-08-14 16:34:43 +0800

    дз 9.4.2

--- add/models.py
+++ add/models.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.db import models
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+user = get_user_model()
 
 class Advert(models.Model):
     title = models.CharField("Заголовок", max_length=128)
@@ -9,6 +12,8 @@ class Advert(models.Model):
     auction = models.BooleanField("Торг", help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(user, verbose_name="Пользователь", on_delete=models.CASCADE)
+    image = models.ImageField("Изображение", upload_to="adverts/")
 
     @admin.display(description='Дата создания')
     def date_of_creation(self):
@@ -20,7 +25,8 @@ class Advert(models.Model):
             )
         return self.created_at.time().strftime("%d.%m.%Y в %H:%M:%S")
     
-    @admin.display(description='Дата создания')
+
+    @admin.display(description='Дата обновления')
     def date_of_update(self):
         from django.utils import timezone
         if self.updated_at.date() == timezone.now().date():
--- add/views.py
+++ add/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Advert
 
 def index(request):
-    return render(request, 'index.html')
+    adverts = Advert.objects.all()
+    context = {"adverts": adverts}
+    return render(request, 'index.html', context)
 
 def top_sellers(request):
     return render(request, 'top-sellers.html')
